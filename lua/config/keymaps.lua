07d8b1115baf565191c9337f047bdee57cd4ee13
Yupu <feng.yu.pu0330@gmail.com>
2025-07-31 17:44:46 +0800

    feat: add basic configs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+vim.keymap.set("n", "<CR>", "o<Esc>", { noremap = true })
+vim.keymap.set("n", "<Left>", ":bprevious<CR>", { noremap = true })
+vim.keymap.set("n", "<Right>", ":bnext<CR>", { noremap = true })
+
+vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true })
+
